9e45314c57e487d16accbc2ffdd348ca8bfbd2b4
Suryansh Parashar <sparashar2002@gmail.com>
2025-05-21 22:47:22 +0530

    Update Vite configuration to change API proxy target to backend URL

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,17 +3,17 @@ import react from "@vitejs/plugin-react"
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    // host: "0.0.0.0",
-    // port: 54000,
-    proxy: {
-      "/api/v1": {
-        target: "https://artha-nirikshan.vercel.app",
-        changeOrigin: true,
-        // rewrite: (path) => path.replace(/^\/api\/v1/, "/api/v1"),
-      }
+    server: {
+        // host: "0.0.0.0",
+        // port: 54000,
+        proxy: {
+            "/api/v1": {
+                target: "https://artha-nirikshan-backend.vercel.app",
+                changeOrigin: true,
+                // rewrite: (path) => path.replace(/^\/api\/v1/, "/api/v1"),
+            },
+        },
+        logLevel: "info",
     },
-    logLevel: "info",
-  },
-  plugins: [react()],
+    plugins: [react()],
 })
